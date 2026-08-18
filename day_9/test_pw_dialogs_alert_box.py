88e428bbf22811a29d6e2c0e49b75d56cca12b67
Shakeel Nawaz <Shakeelnawaz1@gmail.com>
2026-08-18 14:53:17 +0530

    Added Day9 Alert Handling All Types

--- day_9/test_pw_dialogs_alert_box.py
+++ day_9/test_pw_dialogs_alert_box.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import Page
+import pytest
 
 """
 There are 3 Types of Dialogs
@@ -28,15 +29,54 @@ How to Register an Event        ->  By using .on() method,      i.e  .on(<EventT
 
 """
 
+
 def test_simple_dialog(page:Page):
+    """
+    Using lambda x:x.accept()
+    """
+
     page.goto('https://testautomationpractice.blogspot.com/')
 
-    def handle_dialog(dialog):
-        dialog.accept()
-        # dialog.dismiss()
+# 1. Approach 1 (NOT RECOMMENDED)
 
-    page.on("dialog",handle_dialog)
+    # def handle_dialog(dialog):
+    #     dialog.accept()
+
+    # page.on("dialog",handle_dialog)
+    # page.wait_for_timeout(3000)
+    # page.get_by_role('button',name='Simple Alert').click()
+
+# 2. Approach 2 (RECOMMENDED & APPLICABLE TO ALL 3 Types OF ALERTS)
+
+    page.on('dialog',lambda x:x.accept())
+    page.wait_for_timeout(3000)
+    page.get_by_role('button',name='Simple Alert').click()
+
+    page.wait_for_timeout(3000)
+
+def test_confirmation_alert(page:Page):
+    """
+    Using lambda x:x.accept()       to  Accept Confirmation Alert
+    Using lambda x:x.dismiss()      to  Dismiss Confirmation Alert
+    """
+    page.goto('https://testautomationpractice.blogspot.com/')
+
+    page.on('dialog',lambda x:x.accept())
+    # page.on('dialog',lambda x:x.dismiss())
     page.wait_for_timeout(3000)
     page.get_by_role('button',name='Confirmation Alert').click()
+    page.wait_for_timeout(3000)
 
-    page.wait_for_timeout(10000)
+def test_prompt_alert(page:Page):
+    """
+    Using lambda x:x.accept('<InputValue>')       to  Accept Confirmation Alert
+        To pass text inside prompt, we need to add text(string) inside Round Brackets 
+    Using lambda x:x.dismiss()      to  Dismiss Confirmation Alert
+    """
+    page.goto('https://testautomationpractice.blogspot.com/')
+
+    # page.on('dialog',lambda x:x.accept('100000000'))
+    page.on('dialog',lambda x:x.dismiss())
+    page.wait_for_timeout(3000)
+    page.get_by_role('button',name='Prompt Alert').click()
+    page.wait_for_timeout(3000)
